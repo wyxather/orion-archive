910fa0e8467dfe2361b08a65d4bcfeecc47c5e92
wyxather <wyxather@gmail.com>
2022-08-09 18:57:24 +0700

    Add custom string class that automatically encrypt if out of scope.

--- Orion-Internal/Utility.h
+++ Orion-Internal/Utility.h
@@ -2,4 +2,22 @@
 
 namespace Orion
 {
+	template <stb::compiletime_string_wrapper str>
+	class String
+	{
+	public:
+		String(String&&) = delete;
+		String(const String&) = delete;
+		String& operator=(String&&) = delete;
+		String& operator=(const String&) = delete;
+
+	private:
+		static inline auto value{ xorarr(stb::compiletime_value<str()>::value) };
+
+	public:
+		constexpr String() noexcept { value.crypt(); }
+		constexpr ~String() noexcept { value.crypt(); }
+
+		[[nodiscard]] static constexpr auto get() noexcept { return value.get(); }
+	};
 }
--- Orion-Internal/dllmain.cpp
+++ Orion-Internal/dllmain.cpp
@@ -1,5 +1,6 @@
 #include <clocale>
 #include "Orion.h"
+#include "Utility.h"
 
 EXTERN_C BOOL WINAPI _CRT_INIT(HMODULE, DWORD, LPVOID);
 
@@ -10,7 +11,8 @@ BOOL APIENTRY DllEntryPoint(HMODULE moduleHandle, DWORD reason, LPVOID reserved)
 
     if (reason == DLL_PROCESS_ATTACH) {
 
-        std::setlocale(LC_CTYPE, xorstr_(".utf8"));
+        Orion::String<".utf8"> locale;
+        std::setlocale(LC_CTYPE, locale.get());
 
         _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
         _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
