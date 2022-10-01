2f8b6098664678da86eee8110206ccbe004d7c7f
wyxather <wyxather@gmail.com>
2022-10-02 01:20:51 +0700

    Add getFontGlyphRanges method to gui.

--- Orion-Internal/Source/Module/Gui.cpp
+++ Orion-Internal/Source/Module/Gui.cpp
@@ -1770,7 +1770,27 @@ Gui::Gui() noexcept :
 	auto&& colors{ style.Colors };
 	colors[ImGuiCol_WindowShadow] = ImVec4{ 0, 0, 0, 1 };
 
-	m_fonts.defaultFont = m_io.Fonts->AddFontFromMemoryCompressedTTF(museosanscyrl_700_compressed_data, museosanscyrl_700_compressed_size, 15);
+	constexpr auto getFontGlyphRanges = []() noexcept {
+		static ImVector<ImWchar> ranges;
+		if (ranges.empty()) {
+			ImFontGlyphRangesBuilder builder;
+			constexpr ImWchar baseRanges[]{
+				0x0100, 0x024F, // Latin Extended-A + Latin Extended-B
+				0x0300, 0x03FF, // Combining Diacritical Marks + Greek/Coptic
+				0x0600, 0x06FF, // Arabic
+				0x0E00, 0x0E7F, // Thai
+				0
+			};
+			builder.AddRanges(baseRanges);
+			builder.AddRanges(ImGui::GetIO().Fonts->GetGlyphRangesCyrillic());
+			builder.AddRanges(ImGui::GetIO().Fonts->GetGlyphRangesChineseSimplifiedCommon());
+			builder.AddText("\u9F8D\u738B\u2122");
+			builder.BuildRanges(&ranges);
+		}
+		return ranges.Data;
+	};
+
+	m_fonts.defaultFont = m_io.Fonts->AddFontFromMemoryCompressedTTF(museosanscyrl_700_compressed_data, museosanscyrl_700_compressed_size, 15, {}, getFontGlyphRanges());
 	{
 		ImFontConfig cfg;
 		cfg.MergeMode = true;
