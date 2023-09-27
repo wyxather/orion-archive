a1ad3eaef7755b24d0d4b54800008875a47da033
Wyxather <79754485+Wyxather@users.noreply.github.com>
2023-09-27 18:43:42 +0700

    style: Update entry_point.cpp and main.cpp

--- orion/source/entry_point.cpp
+++ orion/source/entry_point.cpp
@@ -4,24 +4,20 @@
 
 EXTERN_C BOOL WINAPI _CRT_INIT(HMODULE, DWORD, LPVOID);
 
-namespace orion {
-
-    auto EntryPoint::process(
-        const HMODULE module_handle,
-        const DWORD reason_for_call,
-        const LPVOID reserved
-    ) noexcept -> BOOL {
-        const auto crt_init_result =
-            _CRT_INIT(module_handle, reason_for_call, reserved);
-        if (crt_init_result == TRUE && reason_for_call == DLL_PROCESS_ATTACH) {
-            context.handle = module_handle;
-            context.console.emplace();
-            context.platform.emplace(std::nullopt, std::nullopt);
-            context.renderer.emplace(Renderer::Enumerate::MANUAL);
-            context.input.emplace(Input::Enumerate::MANUAL);
-            context.platform->hook();
-        }
-        return crt_init_result;
+auto orion::EntryPoint::process(
+    const HMODULE module_handle,
+    const DWORD reason_for_call,
+    const LPVOID reserved
+) noexcept -> BOOL {
+    const auto crt_init_result =
+        _CRT_INIT(module_handle, reason_for_call, reserved);
+    if (crt_init_result == TRUE && reason_for_call == DLL_PROCESS_ATTACH) {
+        context.handle = module_handle;
+        context.console.emplace();
+        context.platform.emplace(std::nullopt, std::nullopt);
+        context.renderer.emplace(Renderer::Enumerate::MANUAL);
+        context.input.emplace(Input::Enumerate::MANUAL);
+        context.platform->hook();
     }
-
-}  // namespace orion
+    return crt_init_result;
+}
--- orion/source/main.cpp
+++ orion/source/main.cpp
@@ -1,9 +1,11 @@
 #include "entry_point.h"
 
+using orion::EntryPoint;
+
 auto APIENTRY DllEntryPoint(
     const HMODULE module_handle,
     const DWORD reason_for_call,
     const LPVOID reserved
 ) -> BOOL {
-    return orion::EntryPoint::process(module_handle, reason_for_call, reserved);
+    return EntryPoint::process(module_handle, reason_for_call, reserved);
 }
