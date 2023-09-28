673f57c22e347ffe8746cbd0358f756e7d314df3
Wyxather <79754485+Wyxather@users.noreply.github.com>
2023-09-28 13:24:13 +0700

    refactor: Use pragma push/pop in framework.h

--- orion/source/framework.h
+++ orion/source/framework.h
@@ -1,11 +1,12 @@
 #pragma once
 
-#ifndef NOMINMAX
-    #define NOMINMAX
-#endif
+#pragma push_macro("NOMINMAX")
+#pragma push_macro("WIN32_LEAN_AND_MEAN")
 
-#ifndef WIN32_LEAN_AND_MEAN
-    #define WIN32_LEAN_AND_MEAN
-#endif
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
 
 #include <Windows.h>
+
+#pragma pop_macro("NOMINMAX")
+#pragma pop_macro("WIN32_LEAN_AND_MEAN")
