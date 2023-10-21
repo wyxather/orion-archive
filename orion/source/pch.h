dc14dc24f89b35d3f24fdb3ec43e61dc2b2c41f1
Wyxather <79754485+Wyxather@users.noreply.github.com>
2023-10-22 05:34:27 +0700

    refactor: Add lazy_importer.hpp to pch.h.

--- orion/source/pch.h
+++ orion/source/pch.h
@@ -6,6 +6,13 @@
 #include <cstddef>
 #include <utility>
 
+#define LAZY_IMPORTER_CASE_INSENSITIVE
+#include "dependencies/lazy_importer.hpp"
+#define LI_MOD_(name)  ::li::detail::lazy_module<LAZY_IMPORTER_KHASH(name)>
+#define LI_MOD(name)   LI_MOD_(name)
+#define LI_FUNC_(name) ::li::detail::lazy_function<LAZY_IMPORTER_KHASH(#name), decltype(&name)>
+#define LI_FUNC(name)  LI_FUNC_(name)
+
 #include "source/framework.h"
 #include "source/utilities/option.h"
 
