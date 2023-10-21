9dffa4349a40a577c1f37b876dd4ff5e58eb5408
Wyxather <79754485+Wyxather@users.noreply.github.com>
2023-10-22 05:50:36 +0700

    chore: Add kernel32.h.

---
+++ orion/source/imports/kernel32.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace orion::imports
+{
+
+struct Kernel32 final
+{
+    Kernel32(Kernel32&&)                 = delete;
+    Kernel32& operator=(Kernel32&&)      = delete;
+    Kernel32(const Kernel32&)            = delete;
+    Kernel32& operator=(const Kernel32&) = delete;
+
+    explicit Kernel32(void* kernel32) noexcept;
+
+    decltype(&AllocConsole) allocConsole = nullptr;
+    decltype(&FreeConsole)  freeConsole  = nullptr;
+};
+
+} // namespace orion::imports
