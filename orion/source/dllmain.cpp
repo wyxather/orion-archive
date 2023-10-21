fd6a1d05412d72cde75df64cd05b28f32cd8d8cf
Wyxather <79754485+Wyxather@users.noreply.github.com>
2023-10-21 16:12:04 +0700

    refactor: Define orion::Main::onAttach method.

--- orion/source/dllmain.cpp
+++ orion/source/dllmain.cpp
@@ -1,7 +1,13 @@
+#include "source/dllmain.h"
+
 EXTERN_C BOOL WINAPI _CRT_INIT(HMODULE, DWORD, LPVOID);
 
 BOOL APIENTRY DllEntryPoint(const HMODULE moduleHandle, const DWORD reason, const LPVOID reserved)
 {
     const auto crtInitResult = _CRT_INIT(moduleHandle, reason, reserved);
     return crtInitResult;
+}
+
+void orion::Main::onAttach(const HMODULE moduleHandle) noexcept
+{
 }
