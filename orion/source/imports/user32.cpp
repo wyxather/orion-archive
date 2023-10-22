19c5f83550a35767b5b3ab4ca878fd2341daff98
Wyxather <79754485+Wyxather@users.noreply.github.com>
2023-10-22 07:33:45 +0700

    chore: Add user32.cpp.

---
+++ orion/source/imports/user32.cpp
@@ -0,0 +1,7 @@
+#include "source/imports/user32.h"
+
+orion::imports::User32::User32(void* const user32) noexcept
+{
+    getWindowLongPtr = LI_FUNC(GetWindowLongPtr)::in(user32);
+    setWindowLongPtr = LI_FUNC(SetWindowLongPtr)::in(user32);
+}
