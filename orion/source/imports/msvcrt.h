9c6dcbbbf3e20ef3ffeaf05061b8b64e3a6ad04d
Wyxather <79754485+Wyxather@users.noreply.github.com>
2023-10-22 08:21:28 +0700

    chore: Add msvcrt.h.

---
+++ orion/source/imports/msvcrt.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace orion::imports
+{
+
+struct Msvcrt final
+{
+    Msvcrt(Msvcrt&&)                 = delete;
+    Msvcrt& operator=(Msvcrt&&)      = delete;
+    Msvcrt(const Msvcrt&)            = delete;
+    Msvcrt& operator=(const Msvcrt&) = delete;
+
+    explicit Msvcrt(void* msvcrt) noexcept;
+
+    int(__cdecl* _vsnprintf_s)(
+        char* buffer, std::size_t sizeOfBuffer, std::size_t count, const char* format, va_list argptr) = nullptr;
+};
+
+} // namespace orion::imports
