3428e1d220b282250b6c75e9c20668ea920eaa9f
Wyxather <79754485+Wyxather@users.noreply.github.com>
2023-10-24 18:10:28 +0700

    chore: Add application.h.

---
+++ orion/source/application.h
@@ -0,0 +1,41 @@
+#pragma once
+
+namespace orion
+{
+
+namespace core
+{
+
+struct Platform;
+
+}
+
+class Application final
+{
+    friend core::Platform;
+
+    Application()                                = delete;
+    ~Application()                               = delete;
+    Application( Application&& )                 = delete;
+    Application& operator=( Application&& )      = delete;
+    Application( const Application& )            = delete;
+    Application& operator=( const Application& ) = delete;
+
+    static void exit() noexcept;
+
+    class Unload final
+    {
+        friend Application;
+
+        Unload()                           = delete;
+        ~Unload()                          = delete;
+        Unload( Unload&& )                 = delete;
+        Unload& operator=( Unload&& )      = delete;
+        Unload( const Unload& )            = delete;
+        Unload& operator=( const Unload& ) = delete;
+
+        [[noreturn]] static void WINAPI unload( LPVOID ) noexcept;
+    };
+};
+
+} // namespace orion
