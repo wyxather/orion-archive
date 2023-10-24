b16e9a314fd3113f227d8b8e06eabbc89db30fe4
Wyxather <79754485+Wyxather@users.noreply.github.com>
2023-10-24 16:33:36 +0700

    refactor: Hide Window::callOriginalProcedure.

--- orion/source/core/platform.h
+++ orion/source/core/platform.h
@@ -33,11 +33,6 @@ struct Platform final
 
         explicit Window( const imports::Kernel32& kernel32, const imports::User32& user32 ) noexcept;
 
-        _NODISCARD LRESULT callOriginalProcedure( HWND   window,
-                                                  UINT   message,
-                                                  WPARAM wParam,
-                                                  LPARAM lParam ) const noexcept;
-
       private:
         static BOOL CALLBACK    enumWindowsProc( HWND window, Window& self ) noexcept;
         static LRESULT CALLBACK procedure( HWND window, UINT message, WPARAM wParam, LPARAM lParam ) noexcept;
@@ -51,6 +46,11 @@ struct Platform final
         _NODISCARD static bool  isVisible( HWND window ) noexcept;
         _NODISCARD static bool  isEqualToCurrentProcessId( DWORD processId ) noexcept;
         _NODISCARD static bool  isConsole( const char* className ) noexcept;
+
+        _NODISCARD LRESULT callOriginalProcedure( HWND   window,
+                                                  UINT   message,
+                                                  WPARAM wParam,
+                                                  LPARAM lParam ) const noexcept;
 
         HWND    handle            = nullptr;
         WNDPROC originalProcedure = nullptr;
