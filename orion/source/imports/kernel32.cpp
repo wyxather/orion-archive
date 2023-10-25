3a43278133edbd26dda9d1b0deb9402395f930e1
Wyxather <79754485+Wyxather@users.noreply.github.com>
2023-10-25 14:14:11 +0700

    refactor: Add Kernel32 to_json method.

--- orion/source/imports/kernel32.cpp
+++ orion/source/imports/kernel32.cpp
@@ -18,3 +18,25 @@ orion::imports::Kernel32::Kernel32( void* const kernel32 ) noexcept
     sleep                    = LI_FUNC( Sleep )::in( kernel32 );
     writeConsoleA            = LI_FUNC( WriteConsoleA )::in( kernel32 );
 }
+
+void orion::imports::to_json( nlohmann::json& json, const Kernel32& kernel32 ) noexcept
+{
+    json = {
+        { xorstr_( "allocConsole" ), reinterpret_cast<std::uintptr_t>( kernel32.allocConsole ) },
+        { xorstr_( "closeHandle" ), reinterpret_cast<std::uintptr_t>( kernel32.closeHandle ) },
+        { xorstr_( "createThread" ), reinterpret_cast<std::uintptr_t>( kernel32.createThread ) },
+        { xorstr_( "freeConsole" ), reinterpret_cast<std::uintptr_t>( kernel32.freeConsole ) },
+        { xorstr_( "freeLibraryAndExitThread" ),
+          reinterpret_cast<std::uintptr_t>( kernel32.freeLibraryAndExitThread ) },
+        { xorstr_( "getConsoleWindow" ), reinterpret_cast<std::uintptr_t>( kernel32.getConsoleWindow ) },
+        { xorstr_( "getCurrentProcessId" ), reinterpret_cast<std::uintptr_t>( kernel32.getCurrentProcessId ) },
+        { xorstr_( "getDateFormatA" ), reinterpret_cast<std::uintptr_t>( kernel32.getDateFormatA ) },
+        { xorstr_( "getLocalTime" ), reinterpret_cast<std::uintptr_t>( kernel32.getLocalTime ) },
+        { xorstr_( "getStdHandle" ), reinterpret_cast<std::uintptr_t>( kernel32.getStdHandle ) },
+        { xorstr_( "getTimeFormatA" ), reinterpret_cast<std::uintptr_t>( kernel32.getTimeFormatA ) },
+        { xorstr_( "setConsoleCtrlHandler" ), reinterpret_cast<std::uintptr_t>( kernel32.setConsoleCtrlHandler ) },
+        { xorstr_( "setConsoleTextAttribute" ), reinterpret_cast<std::uintptr_t>( kernel32.setConsoleTextAttribute ) },
+        { xorstr_( "sleep" ), reinterpret_cast<std::uintptr_t>( kernel32.sleep ) },
+        { xorstr_( "writeConsoleA" ), reinterpret_cast<std::uintptr_t>( kernel32.writeConsoleA ) },
+    };
+}
--- orion/source/imports/kernel32.h
+++ orion/source/imports/kernel32.h
@@ -5,6 +5,8 @@ namespace orion::imports
 
 struct Kernel32 final
 {
+    friend void to_json( nlohmann::json& json, const Kernel32& kernel32 ) noexcept;
+
     Kernel32( Kernel32&& )                 = delete;
     Kernel32& operator=( Kernel32&& )      = delete;
     Kernel32( const Kernel32& )            = delete;
