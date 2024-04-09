e3562137b05df12e5a854e527a150cb6a056b91f
wyxather <79754485+wyxather@users.noreply.github.com>
2024-04-09 10:30:35 +0700

    build: Add imgui.

--- orion/pch.h
+++ orion/pch.h
@@ -1,4 +1,31 @@
 #ifndef PCH_H
 #define PCH_H
 
+#ifdef __cplusplus
+    #define IMGUI_DEFINE_MATH_OPERATORS
+    #define IM_STRV_CLASS_EXTRA \
+        constexpr explicit ImStrv(const char *const str, const int size) noexcept { \
+            Begin = str; \
+            End = str + size; \
+        } \
+        template<typename XorArr> \
+        constexpr explicit ImStrv(const XorArr &xor_arr) noexcept { \
+            Begin = xor_arr.data(); \
+            End = Begin + (xor_arr.size() - 1); \
+        }
+    #define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
+    #define IMGUI_DISABLE_WIN32_DEFAULT_CLIPBOARD_FUNCTIONS
+    #define IMGUI_DISABLE_WIN32_DEFAULT_IME_FUNCTIONS
+    #define IMGUI_DISABLE_WIN32_FUNCTIONS
+    #define IMGUI_DISABLE_FILE_FUNCTIONS
+    #define IMGUI_DISABLE_DEFAULT_ALLOCATORS
+    #define IMGUI_IMPL_WIN32_DISABLE_GAMEPAD
+    #ifdef NDEBUG
+        #define IMGUI_DISABLE_DEMO_WINDOWS
+        #define IMGUI_DISABLE_DEBUG_TOOLS
+    #endif
+    #include "dependency/imgui/imgui.h"
+    #include "dependency/imgui/imgui_internal.h"
+#endif
+
 #endif  //PCH_H
